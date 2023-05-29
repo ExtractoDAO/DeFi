e561c20301bff4725ae49fd62fb723b045e62651
Lucas Oliveira <olivmath@protonmail.com>
2023-05-29 16:26:31 -0300

    remove: unused code

--- smartcontracts/src/token/ERC20.sol
+++ smartcontracts/src/token/ERC20.sol
@@ -102,27 +102,3 @@ abstract contract ERC20 {
         emit Transfer(address(0), to, amount);
     }
 }
-
-contract COW is ERC20 {
-    address immutable owner;
-    address dao;
-
-    constructor() ERC20("Cow", "COW", 18) {
-        owner = msg.sender;
-    }
-
-    function setDao(address newDao) public {
-        require(msg.sender == owner, "UNAUTHORIZED");
-        require(msg.sender != address(0x0), "ZERO_ADDRESS");
-        require(newDao != address(0x0), "ZERO_ADDRESS");
-
-        dao = newDao;
-    }
-
-    function pay(address investor, uint256 amount) public {
-        require(msg.sender == dao, "UNAUTHORIZED");
-        require(msg.sender != address(0x0), "ZERO_ADDRESS");
-
-        _mint(investor, amount);
-    }
-}
