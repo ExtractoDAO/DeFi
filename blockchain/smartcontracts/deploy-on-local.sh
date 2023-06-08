b343405b3856c9144f8d497ce3ff1b5188ee108f
Lucas Oliveira <olivmath@protonmail.com>
2023-06-08 12:11:17 -0300

    update: deploy updating

---
+++ blockchain/smartcontracts/deploy-on-local.sh
@@ -0,0 +1,10 @@
+forge b --skip test script
+
+source .env
+
+forge script scripts/deploy.e2e.sol:DeployE2E \
+    --rpc-url http://127.0.0.1:8545 \
+    --broadcast \
+    --optimize
+
+python3 deploy.py
