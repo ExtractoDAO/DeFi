f489f36fcd4a92a11eeecb73ae0ef84a7352f166
Lucas Oliveira <olivmath@protonmail.com>
2023-06-08 22:03:46 -0300

    fix: import

--- blockchain/smartcontracts/test/commodity/Drawer/StableCoinsFuture.t.sol
+++ blockchain/smartcontracts/test/commodity/Drawer/StableCoinsFuture.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-import {Commodity} from "../../../src/extracto/commodity/Commodity.sol";
-import {Future} from "../../../src/extracto/future/Future.sol";
+import {Commodity} from "../../../src/extracto/facet/commodity/Commodity.sol";
+import {Future} from "../../../src/extracto/facet/future/Future.sol";
 import {BaseSetup} from "../../BaseSetup.t.sol";
 import {MockToken} from "../../MockToken.t.sol";
 
