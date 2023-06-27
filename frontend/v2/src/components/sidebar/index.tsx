4176a3c536bc2eeff1e0e6003f5f03667817b7e3
Robson Santana <robson.miranda1991@gmail.com>
2023-06-27 15:47:24 -0300

    feat(components): create sidebar component

---
+++ frontend/v2/src/components/sidebar/index.tsx
@@ -0,0 +1,34 @@
+"use client"
+import { ReactNode, useState } from "react"
+import classNames from "classnames"
+import { Bars3Icon } from "@heroicons/react/24/outline"
+
+interface SidebarProps {
+    children: ReactNode
+}
+
+export default function Sidebar({ children }: SidebarProps) {
+    const [collapsed, setSidebarCollapsed] = useState(true)
+
+    return (
+        <aside
+            className={classNames({
+                // 👇 use grid layout
+                "grid min-h-screen": true,
+                // 👇 toggle the width of the sidebar depending on the state
+                "grid-cols-sidebar": !collapsed,
+                "grid-cols-sidebar-collapsed": collapsed,
+                // 👇 transition animation classes
+                "transition-[grid-template-columns] duration-300 ease-in-out":
+                    true
+            })}
+        >
+            <div className="bg-white text-gray/500">
+                <button onClick={() => setSidebarCollapsed((prev) => !prev)}>
+                    <Bars3Icon className="w-10 h-10" />
+                </button>
+            </div>
+            {children}
+        </aside>
+    )
+}
