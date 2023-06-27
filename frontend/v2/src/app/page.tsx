3040d630a8a1cc99327c4de30e1c22594d15c5b7
Robson Santana <robson.miranda1991@gmail.com>
2023-06-26 23:01:04 -0300

    feat: renamed webapp to frontend in this folder

---
+++ frontend/v2/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image"
+
+export default function Home() {
+    return (
+        <main>
+            <div>
+                <h2 className="text-brand/secondary/500 dark:text-brand/primary/500">
+                    Hello world
+                </h2>
+            </div>
+        </main>
+    )
+}
---
+++ frontend/v2/tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: "class",
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+    ],
+    theme: {
+        colors: {
+            // Base
+            white: "#FFFFFF",
+            "base/white": "#FFFFFF",
+            "brand/primary/500": "#F9712B",
+            "brand/secondary/100": "#DCD5E3",
+            "brand/secondary/500": "#522B72",
+            // Gray
+            "Default/gray/50": "#F9FAFB",
+            "Default/gray/200": "#E5E7EB",
+            "gray/200": "#E4E6EA",
+            "gray/400": "#9BA2AE",
+            "gray/500": "#6B7280",
+            "Default/gray/500": "#6B7280",
+            "gray/600": "#4B5563",
+            "cool-gray/900": "#111827",
+            "Default/gray/900": "#111827",
+            "gray/900": "#131313",
+            // Slate
+            "slate/100": "#F0F4F8",
+            "slate/50": "#F7F9FB",
+            "slate/700": "#334155",
+            // Red
+            "red/500": "#EE4444",
+            // Green
+            "Default/green/100": "#D1FAE5",
+            "Default/green/800": "#065F46"
+        }
+    },
+    plugins: []
+}
