676b75ebaedcf6ad3a5ec0395528f652063fe1fa
Robson Santana <robson.miranda1991@gmail.com>
2023-07-04 19:51:09 -0300

    feat(buy): value input

--- frontend/v2/src/components/buy/index.tsx
+++ frontend/v2/src/components/buy/index.tsx
@@ -1,5 +1,24 @@
+import ValueInput from "../valueInput"
+
 export default function Buy() {
+    const TokenSelector = () => (
+        <select name="token" id="selectToken">
+            <option value="usdt">Tether</option>
+            <option value="usdc">USD Coin</option>
+            <option value="busd">Binance USD</option>
+            <option value="dai">DAI</option>
+        </select>
+    )
+
     return (
-        <div className="max-md:w-full max-w-2xl p-6 bg-white dark:bg-deep-gray/100 mx-auto"></div>
+        <div className="max-md:w-full max-w-2xl p-6 bg-white dark:bg-deep-gray/100 mx-auto">
+            <ValueInput
+                label="From"
+                insideElement={{
+                    element: TokenSelector
+                }}
+            />
+            <ValueInput label="To (Estimated)" />
+        </div>
     )
 }
