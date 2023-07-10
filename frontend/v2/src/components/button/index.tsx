523cec7858572456ae0fb2a5bf45d35d22cca569
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-07-10 14:08:49 -0300

    Frontend/feat/primary button (#57)
    * feat(button):Button component and its properties, conditional rendering logic for the Button variant, ButtonProps to add icon support
    
    * feat(button):add new Primary Button component
    
    * feat(button):add new Primary Button Disabled component
    
    * feat(button):add new secondary component Button
    
    * feat(button):add new secondary component Button Disabled
    
    * feat(button):variant options for Button component, switch cases for different variants on Button component
    
    * feat(button):add constant to store default style
    
    * feat(button):Add default styling to button components in switch-case
    
    * feat(button): add constants for children, iconleft, iconright and defaultStyle properties
    
    * feat(button):Added in all components conditional for support for icons and text in the Button component
    
    * chore(button):icon stylization
    
    * chore(button):primary button styling
    
    * chore(button):disabled primary button styling
    
    * chore(button):secondary button styling
    
    * chore(button):disabled secondary button styling
    
    * chore(tailwind-config):added new colors
    
    * fix(button):correction of the file name referring to the Button component from button.tsx to index.tsx thus following the pattern
    
    * chore(button):add style for dark mode
    
    * chore(button):add new colors
    
    ---------
    
    Co-authored-by: Robson Santana <robson.miranda1991@gmail.com>

---
+++ frontend/v2/src/components/button/index.tsx
@@ -0,0 +1,153 @@
+import {
+    ButtonHTMLAttributes,
+    ReactElement,
+    ReactNode,
+    cloneElement,
+} from "react"
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    children: ReactNode
+    iconleft?: ReactElement
+    iconright?: ReactElement
+    variant?:
+        | "primary"
+        | "secondary"
+        | "primdisabled"
+        | "secdisabled"
+    defaultStyle?: string
+}
+
+export const Button = (props: ButtonProps) => {
+    const { variant } = props
+
+    const style =
+        "flex inline-flex py-2 px-4 items-center gap-[10px] text-sm not-italic font-medium rounded-[4px]"
+
+        switch (variant) {
+            case "secondary":
+                return <SecondaryButton defaultStyle={style} {...props} />
+            case "secdisabled":
+                return <SecBtDisabled defaultStyle={style} {...props} />
+            case "primdisabled":
+                return <PrimBtDisabled defaultStyle={style} {...props} />
+            default:
+                return <PrimaryButton defaultStyle={style} {...props} />
+        }
+    }
+
+export const PrimaryButton = (props: ButtonProps) => {
+    const { children, iconleft, iconright, defaultStyle } = props
+
+    return (
+        <button
+        {...props}
+            className={`${defaultStyle} 
+                text-white 
+                bg-brand/secondary/500 
+                hover:bg-brand/secondary/600
+                  focus:outline-none 
+                  focus:ring-2 
+                focus:ring-brand/secondary/300 
+                  focus:border-transparent 
+                  focus:border
+
+                dark:bg-brand/primary/500 
+                dark:text-deep-gray/100
+                `}
+        >
+            {iconright &&
+                cloneElement(iconright, {
+                    className: "w-[16px] h-[16px] shrink-0"
+                })}
+            <span>{children}</span>
+            {iconleft &&
+                cloneElement(iconleft, {
+                    className: "w-[16px] h-[16px] shrink-0"
+                })}
+        </button>
+    )
+}
+
+export const PrimBtDisabled = (props: ButtonProps) => {
+    const { children, iconleft, iconright, defaultStyle } = props
+
+    return (
+        <button
+            {...props}
+            className={`${defaultStyle} 
+                bg-brand/secondary/100
+                text-brand/secondary/300
+                  cursor-default
+                `}
+        >
+            {iconright &&
+                cloneElement(iconright, {
+                    className: "w-[16px] h-[16px] shrink-0"
+                })}
+            <span>{children}</span>
+            {iconleft &&
+                cloneElement(iconleft, {
+                    className: "w-[16px] h-[16px] shrink-0"
+                })}
+        </button>
+    )
+}
+
+export const SecondaryButton = (props: ButtonProps) => {
+    const { children, iconleft, iconright, defaultStyle } = props
+
+    return (
+        <button
+            {...props}
+            className={`${defaultStyle} 
+                    border 
+                    border-solid 
+                  border-gray/200 
+                  bg-white
+                  hover:border-gray/300
+                  focus:border-gray/800
+
+                  dark:text-gray/250
+                  dark:bg-deep-gray/100
+                  dark:border-gray/350
+                  dark:hover:border-gray/400
+                    
+                  `}
+        >
+            {iconright &&
+                cloneElement(iconright, {
+                    className: "w-[16px] h-[16px] shrink-0"
+                })}
+            <span>{children}</span>
+            {iconleft &&
+                cloneElement(iconleft, {
+                    className: "w-[16px] h-[16px] shrink-0"
+                })}
+        </button>
+    )
+}
+
+export const SecBtDisabled = (props: ButtonProps) => {
+    const { children, iconleft, iconright, defaultStyle } = props
+
+    return (
+        <button
+            {...props}
+            className={`${defaultStyle} 
+                   bg-gray/200
+                   text-gray/500
+                     cursor-default
+                  `}
+        >
+            {iconright &&
+                cloneElement(iconright, {
+                    className: "w-[16px] h-[16px] shrink-0"
+                })}
+            <span>{children}</span>
+            {iconleft &&
+                cloneElement(iconleft, {
+                    className: "w-[16px] h-[16px] shrink-0"
+                })}
+        </button>
+    )
+}
