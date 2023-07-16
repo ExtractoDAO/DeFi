c2dee9e15d73559611cd620b9246a1e73419b8e7
Lucas Oliveira <olivmath@protonmail.com>
2023-07-16 11:47:07 -0300

    remove: unused code

--- backend/chain_vission/login/router/nonce.py
+++ backend/chain_vission/login/router/nonce.py
@@ -1,19 +1,13 @@
 from fastapi.responses import PlainTextResponse
 from chain_vission.login.router import router
 from chain_vission import memory_pool
-from pydantic import BaseModel
 from fastapi import status
 import siwe
-
-
-class Wallet(BaseModel):
-    address: str
 
 
 @router.get(path="/nonce/{address}", status_code=status.HTTP_201_CREATED)
 async def get_nonce(address: str):
     nonce: str = siwe.generate_nonce()
     memory_pool[address] = nonce
-    print(memory_pool)
 
     return PlainTextResponse(status_code=status.HTTP_201_CREATED, content=nonce)
