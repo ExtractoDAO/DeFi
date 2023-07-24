dd5aacd3b55006737aff54ade0a3e50c97cad47f
Lucas Oliveira <olivmath@protonmail.com>
2023-07-24 01:22:45 -0300

    add: routes for nonoce, sig in/out

---
+++ backend/chain_vission/login/router/signout.py
@@ -0,0 +1,16 @@
+from chain_vission.login.router import router
+from fastapi.responses import JSONResponse
+from chain_vission import adapter_app
+from fastapi import status, Header
+
+
+@router.get(path="/signout/{address}", status_code=status.HTTP_200_OK)
+async def signout(address: str, x_authorization: str = Header(None)):
+    token = adapter_app.get_data(f"/tokens/{address}")
+
+    if token != x_authorization or token is None:
+        return JSONResponse(
+            status_code=status.HTTP_401_UNAUTHORIZED, content={"message": "Unauthorized"}
+        )
+    adapter_app.delete_data(f"/tokens/{address}")
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"message": True})
