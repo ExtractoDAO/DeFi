db89cadc12fae66513a20225b71823a097d95776
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-07-23 23:01:12 -0300

    chore(modal):remove unnecessary div

--- frontend/v2/src/components/modal/index.tsx
+++ frontend/v2/src/components/modal/index.tsx
@@ -97,21 +97,19 @@ export default function Modal({
                                         className="sm:flex 
                                 sm:items-start"
                                     >
-                                        <div>
-                                            {icon && (
-                                                <div
-                                                    className={classNames(
-                                                        "mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full sm:mx-0 sm:h-10 sm:w-10",
-                                                        {
-                                                            [`${iconBgColor}`]:
-                                                                iconBgColor
-                                                        }
-                                                    )}
-                                                >
-                                                    {icon}
-                                                </div>
-                                            )}
-                                        </div>
+                                        {icon && (
+                                            <div
+                                                className={classNames(
+                                                    "mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full sm:mx-0 sm:h-10 sm:w-10",
+                                                    {
+                                                        [`${iconBgColor}`]:
+                                                            iconBgColor
+                                                    }
+                                                )}
+                                            >
+                                                {icon}
+                                            </div>
+                                        )}
                                         <div
                                             className="mt-3 
                                             gap-2
