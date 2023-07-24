e91f0d3424413ecff51806eeaeaf4a038aba302f
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-07-23 21:02:57 -0300

    feat(modal):add transition component and dialog with input and output transition settings

--- frontend/v2/src/components/modal/index.tsx
+++ frontend/v2/src/components/modal/index.tsx
@@ -5,10 +5,29 @@ import Button from "../button"
 import classNames from "classnames"
 
 const Modal = () => {
-  return (
-    <div>
-      <h1>Modal</h1>
-    </div>
-  );
+    return (
+        <Transition.Root show={true} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={() => {}}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                ></Transition.Child>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    enterTo="opacity-100 translate-y-0 sm:scale-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                    leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                ></Transition.Child>
+            </Dialog>
+        </Transition.Root>
+    )
 }
 export default Modal
