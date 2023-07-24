27fbc44fa63321fcf1a26bc825928884b02060e5
Lucas Oliveira <olivmath@protonmail.com>
2023-07-24 02:03:35 -0300

    update: split shema file into mutation and query

---
+++ backend/chain_vission/use_cases/graphql/mutation.py
@@ -0,0 +1,39 @@
+from chain_vission.utils.authentication import verify_token
+from chain_vission.domain.contract import Contract
+from chain_vission import adapter_app
+from strawberry.types import Info
+import strawberry
+
+
+@strawberry.type
+class Response:
+    success: bool = True
+    message: str
+
+
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    def new_contract(
+        self,
+        address: str,
+        commodity_amount: float,
+        locktime: int,
+        owner: str,
+        price: int,
+        info: Info,
+    ) -> Response:
+        token = info.context["request"].state.token
+        if (message := verify_token(token)) is not None:
+            return Response(message=message, success=False)
+
+        contract = Contract(
+            address=address,
+            burn=False,
+            kg=commodity_amount,
+            locktime=locktime,
+            owner=owner,
+            price=price,
+        )
+        adapter_app.set_data(f"/contracts/{contract.address}", contract.__dict__)
+        return Response()
