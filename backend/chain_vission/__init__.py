a916229e64c1ed50aee081e0afc36419f3969a24
Lucas Oliveira <olivmath@protonmail.com>
2023-07-24 00:51:03 -0300

    update: init

--- backend/chain_vission/__init__.py
+++ backend/chain_vission/__init__.py
@@ -2,15 +2,16 @@ from chain_vission.adapter.firebase import FirebaseAdapter
 from fastapi import FastAPI
 import os
 
-memory_pool = dict()
 adapter_app = FirebaseAdapter()
-app = FastAPI()
+app = FastAPI(docs_url="/")
 
 if os.getenv("ENV") == "devnet":
     from fastapi.middleware.cors import CORSMiddleware
 
     origins = [
+        "http://localhost",
         "http://localhost:8080",
+        "http://localhost:8000",
     ]
 
     app.add_middleware(
