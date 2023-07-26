7ed300067498439e30481f3edb1f0aecf99d2bbd
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-07-26 08:26:09 -0300

    chore(sidemodal):rename Contract Visualization to Side modal

--- frontend/v2/src/components/contractVisualization/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const ContractVisualization = () => {
-  return (
-    <div>
-      <h1>ContractVisualization</h1>
-    </div>
-  );
-}
---
+++ frontend/v2/src/components/sideModal/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SideModal = () => {
+  return (
+    <div>
+      <h1>SideModal</h1>
+    </div>
+  );
+}
