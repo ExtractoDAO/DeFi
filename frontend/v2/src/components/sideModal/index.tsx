215280c24e3d3bc1f77f67fe238bfb6d649ee59b
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-07-26 09:13:37 -0300

    feat(sidemodal):Imported required dependencies and components

--- frontend/v2/src/components/sideModal/index.tsx
+++ frontend/v2/src/components/sideModal/index.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+"use client"
+import { Fragment, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
 
 export const SideModal = () => {
   return (
