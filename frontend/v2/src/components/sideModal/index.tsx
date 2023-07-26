cf51b66972586b0693e7df9173efc2df38a7a442
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-07-26 09:20:29 -0300

    feat(sidemodal):useState to manage the state allowing it to be opened and closed

--- frontend/v2/src/components/sideModal/index.tsx
+++ frontend/v2/src/components/sideModal/index.tsx
@@ -3,7 +3,9 @@ import { Fragment, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 
-export const SideModal = () => {
+export default function SideModal() {
+  const [open, setOpen] = useState(true)
+  
   return (
     <div>
       <h1>SideModal</h1>
