bb56a954e62afdfbe3cc79078c76ca9f84545b60
Robson Miranda <46539960+robson036@users.noreply.github.com>
2023-07-27 16:04:19 -0300

    Frontend/feat/api service base (#103)
    * feat: graphql client
    
    * feat: api base
    
    graphql api base

--- frontend/v2/src/app/page.tsx
+++ frontend/v2/src/app/page.tsx
@@ -1,4 +1,21 @@
-export default function Home() {
+// import { gql } from "@apollo/client"
+// import { getClient } from "@/lib/client"
+
+// const client = getClient()
+
+export default async function Home() {
+    // const query = gql`
+    //     query {
+    //         launchLatest {
+    //             mission_name
+    //         }
+    //     }
+    // `
+
+    // const { data } = await getClient().mutate({ mutation:  })
+
+    // console.log(process.env.GRAPHQL_URL)
+
     return (
         <main>
             <h2 className="text-brand/secondary/500 dark:text-brand/primary/500">
