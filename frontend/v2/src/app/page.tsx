7bc110d146758441215d31231ddca04b174ef756
Robson Santana <robson.miranda1991@gmail.com>
2023-07-27 15:20:36 -0300

    feat: graphql client

--- frontend/v2/src/app/page.tsx
+++ frontend/v2/src/app/page.tsx
@@ -1,4 +1,21 @@
-export default function Home() {
+import { gql } from "@apollo/client"
+import { getClient } from "@/lib/client"
+
+const client = getClient()
+
+export default async function Home() {
+    const query = gql`
+        query {
+            launchLatest {
+                mission_name
+            }
+        }
+    `
+
+    const { data } = await getClient().query({ query: query })
+
+    console.log(data)
+
     return (
         <main>
             <h2 className="text-brand/secondary/500 dark:text-brand/primary/500">
