33e701c323404a693718064446f7d19c0723008a
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-07-31 22:48:50 -0300

    feat(tablecontacts):added sidemodal component and function to open sidemodal

--- frontend/v2/src/components/tableContracts/index.tsx
+++ frontend/v2/src/components/tableContracts/index.tsx
@@ -1,6 +1,11 @@
+"use client"
+import { useState } from 'react'
 import { DocumentDuplicateIcon } from "@heroicons/react/24/outline"
+import SideModal from "../sideModal";
 
 function TableContracts() {
+    const [sideModalOpen, setSideModalOpen] = useState(false);
+
     const count = new Array(6).fill("")
 
     const Pill = () => {
@@ -45,7 +50,9 @@ function TableContracts() {
                     <Pill />
                 </td>
                 <td className="px-6 py-4 text-gray/500">
-                    <button className="text-brand/secondary/500 font-medium text-sm dark:text-brand/primary/500">
+                    <button 
+                    onClick={() => setSideModalOpen(true)}
+                    className="text-brand/secondary/500 font-medium text-sm dark:text-brand/primary/500">
                         View
                     </button>
                 </td>
@@ -85,6 +92,16 @@ function TableContracts() {
                     })}
                 </tbody>
             </table>
+            <SideModal
+            contractType='Extract@'
+            contract='ExtractoDAO is a privately owned Decentralized Autonomous Organization, sometimes called Decentralized Autonomous Corporation, owner of shared purpose blockchain and manager of a collective smart contract by DAO on Drawer technology that stores several individual smart contracts under the responsibility of each participant that is the contract auditor and issuer of the stablecon COW; And for the purposes of this document represented by the owner Joel Almeida creator, holder, issuer and holder In compliance with all laws of this country. ExtractoDAO It is virtually located in the Domain www.extractodao.com. ExtractoDAO is the rightful issuer of the ExtractoDAO Bull XBLL governance token contract 0x8110706a399D457D67b7A2B7636482b4bfCeBB21, issuer and keeper of the BULL tokens (XBLL)
+
+            On the other hand, the holder of this smart contract is the legal administrator of the private keys and is fully responsible for managing the funds and generating the COW that will be private and sent back to the original wallet.
+            Smart contract address: 0x0b32337D35f8CAB81180b031D9A244E088d0c926' 
+            contractTitle='contractTitle' 
+            title='My Contract 01' 
+            contractAddress='0x0b32337D35f8CAB81180b031D9A244E088d0c926'  
+            onClose={() => setSideModalOpen(false)} open={sideModalOpen} />
         </div>
     )
 }
