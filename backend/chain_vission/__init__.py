4eba49e3a7479a673ea4551c39fc8570e4c362b1
Lucas Oliveira <50037567+olivmath@users.noreply.github.com>
2023-08-07 01:41:42 -0300

    add: environment modulo into init app

--- backend/chain_vission/__init__.py
+++ backend/chain_vission/__init__.py
@@ -1,26 +1,26 @@
 from chain_vission.middleware.authentication import get_authentication_token
 from chain_vission.adapter.firebase import FirebaseAdapter
+from chain_vission.utils.environment import Environment
 from chain_vission.logger.logs import CustomLogger
-from dotenv import load_dotenv
 from fastapi import FastAPI
-import os
 
-load_dotenv(".env")
-
-logger = CustomLogger("logging.log")
-adapter_app = FirebaseAdapter()
-app = FastAPI(docs_url="/", version"0.4.0")
+env = Environment()
+logger = CustomLogger(env.LOG_PATH)
+adapter_app = FirebaseAdapter(env)
+app = FastAPI(
+    title="Backend",
+    version="0.4.0",
+    redoc_url="/"
+)
 app.middleware("http")(get_authentication_token)
 
-JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-DOMAIN = os.getenv("DOMAIN")
 
-if os.getenv("ENV") == "devnet":
+if env.ENV == "devnet":
     from fastapi.middleware.cors import CORSMiddleware
 
-    JWT_SECRET_KEY = "JWT_SECRET_KEY"
-    DOMAIN = "localhost:8080"
-    logger = CustomLogger("logging.log", True)
+    env.JWT_SECRET_KEY = "JWT_SECRET_KEY"
+    env.DOMAIN = "localhost:8080"
+    logger = CustomLogger(env.LOG_PATH, True)
 
     origins = [
         "http://localhost",
