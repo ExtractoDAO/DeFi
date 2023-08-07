2ecede401fa67275e8cf5d0e8d0ed3ff87d58752
Lucas Oliveira <50037567+olivmath@users.noreply.github.com>
2023-08-07 01:43:25 -0300

    add: environment vars module that crash app if not load all vars (#108)

--- backend/chain_vission/utils/authentication.py
+++ backend/chain_vission/utils/authentication.py
@@ -1,10 +1,10 @@
-from chain_vission import JWT_SECRET_KEY, adapter_app
+from chain_vission import env, adapter_app
 from jose import jwt, JWTError
 from typing import Optional
 
 def verify_token(token: str) -> Optional[str]:
     try:
-        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=["HS256"])
+        payload = jwt.decode(token, env.JWT_SECRET_KEY, algorithms=["HS256"])
     except JWTError as e:
         return f"Authentication attempt rejected: {e}"
 
@@ -14,4 +14,3 @@ def verify_token(token: str) -> Optional[str]:
     if stored_token is None or stored_token != token:
         return "Authentication attempt rejected: Invalid token"
 
-    return None
---
+++ backend/chain_vission/utils/environment.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel, validator
+from dotenv import load_dotenv
+import os
+
+
+class MissingEnvironmentVarError(Exception):
+    def __init__(self, name: str) -> None:
+        super().__init__(f"Env var: '{name}' not found!\nAPPLICATION NEED THIS FOR START")
+
+
+class Environment(BaseModel):
+    AUTH_PROVIDER_X509_CERT_URL: str
+    CLIENT_X509_CERT_URL: str
+    PRIVATE_KEY_ID: str
+    CLIENT_EMAIL: str
+    PRIVATE_KEY: str
+    PROJECT_ID: str
+    TOKEN_URI: str
+    CLIENT_ID: str
+    BASE_URL: str
+    AUTH_URI: str
+    TYPE: str
+
+    JWT_SECRET_KEY: str
+    SIWE_STATEMENT: str
+    SIWE_VERSION: str
+    CHAIN_ID: str
+    LOG_PATH: str
+    DOMAIN: str
+    ENV: str
+
+    @validator("*", pre=True)
+    def check_none(cls, value, field):
+        if value is None:
+            raise MissingEnvironmentVarError(field.name)
+        return value
+
+    def __init__(self, **data):
+        load_dotenv()
+
+        for field in self.__annotations__:
+            data[field] = os.getenv(field)
+        super().__init__(**data)
