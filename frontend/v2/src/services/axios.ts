11557361ce90a668190745248e217623e7a1479a
Lucas Oliveira <50037567+olivmath@users.noreply.github.com>
2023-08-17 15:21:34 -0300

    add: axios services for easy consume (#110)

---
+++ frontend/v2/src/services/axios.ts
@@ -0,0 +1,43 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import { Environment } from "./environment";
+
+export default class AxiosService {
+    private instance: AxiosInstance;
+    
+    constructor(env: Environment) {
+        this.instance = axios.create({
+            baseURL: env.BACKEND_ADDRESS,
+            timeout: 5000,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        this.initializeResponseInterceptor();
+    }
+    private handleResponse = ({ data }: AxiosResponse) => data;
+    private handleError = (error: any) => Promise.reject(error);
+    private initializeResponseInterceptor = () => {
+        this.instance.interceptors.response.use(
+            this.handleResponse,
+            this.handleError,
+        );
+    };
+
+    public async get(url: string, config?: AxiosRequestConfig) {
+        try {
+            const response = await this.instance.get(url, config);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+    
+    public async post(url: string, data?: any, config?: AxiosRequestConfig) {
+        try {
+            const response = await this.instance.post(url, data, config);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
