76eaf9499d459535360d035c373f544669f1573b
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-08-17 15:43:31 -0300

    Frontend/feat/contract side component visualization (#105)
    * feat(ContractVisualization):new ContractVisualization component
    
    * chore(sidemodal):rename Contract Visualization to Side modal
    
    * feat(sidemodal):Imported required dependencies and components
    
    * feat(sidemodal):useState to manage the state allowing it to be opened and closed
    
    * feat(sidemodal):sidemodal with transition and overlay animations focus for sidemodal
    
    * feat(sidemodal):Added the SideModalProps type to define the expected props for the component, and now the onClose prop is used to control the closing of the sidemodal
    
    * feat(sidemodal):button component import and styling
    
    * feat(sidemodal):added container to receive contract
    
    * feat(sidemodal):component link import and styling
    
    * feat(sidemodal):dialogtitle styling, also added children to the container to improve styling
    
    * chore(sidemodal):changes made to improve responsiveness
    
    * chore(sidemodal):add dark style and change indentation for better tailwind reading
    
    * feat(sidemodal):add necessary properties to pass content to component
    
    * feat(chore):working on mobile responsiveness
    
    * feat(sidemodal):working on mobile responsiveness
    
    * Revert "feat(chore):working on mobile responsiveness"
    
    This reverts commit fc0cf6bed89751f0fae3afc285fbdf20417c1767.
    
    * feat(sidemodal):working on mobile responsiveness
    
    * Revert "feat(sidemodal):working on mobile responsiveness"
    
    This reverts commit 620d03db08173652e8ba419e575e2ec7160ab3a9.
    
    * chore(sidemodal):Changes made to improve responsiveness
    
    * feat(sidemodal):Improved closing action and responsiveness
    
    * chore(tailwind.config):new color
    
    * chore(sidemodal):removed test text
    
    * feat(sidemodal):transition out and in to mobile
    
    * feat(tablecontacts):added sidemodal component and function to open sidemodal
    
    * fix(sidemodal):correction in the transition, borders and size of the component according to responsiveness
    
    * chore(sidemodal):Improved classname indentation
    
    * chore(sidemodal):background color correction and start of scrollbar customization
    
    * chore(tailwind.confi): new color
    
    * feat(globals.css):global styling for scrollbar
    
    * feat(sidemodal):added custom scrollbar styling and sidemodal background improvement
    
    ---------
    
    Co-authored-by: Robson <robson.miranda1991@gmail.com>

--- frontend/v2/tailwind.config.js
+++ frontend/v2/tailwind.config.js
@@ -46,7 +46,7 @@ module.exports = {
             "gray/900": "#1F2228",
             "cool-gray/900": "#111827",
             "Default/gray/900": "#111827",
-            "gray/900": "#131313",
+            "gray/sidemodal/900": "#131313",
 
             "deep-gray/100": "#0D0D0D",
             "deep-gray/200": "#202329",
@@ -93,7 +93,10 @@ module.exports = {
             "error/500": "#EF4444",
             "error/600": "#DC2626",
             "error/700": "#B91C1C",
-            "error/800": "#991B1B"
+            "error/800": "#991B1B",
+
+            "dark/purple/600": "#31263A"
+
         },
         data: {
             tokenSelectOpen: 'ui~="tokenSelectOpen"'
