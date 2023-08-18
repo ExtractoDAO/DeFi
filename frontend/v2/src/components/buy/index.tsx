5cb3b15e843c8f020021eb5609aa3bf92a6d8dce
Robson <robson.miranda1991@gmail.com>
2023-08-17 21:21:20 -0300

    feat: buy contract

--- frontend/v2/src/components/buy/index.tsx
+++ frontend/v2/src/components/buy/index.tsx
@@ -7,14 +7,24 @@ import {
     ChartBarIcon,
     DocumentTextIcon,
     ArrowsUpDownIcon
-} from "@heroicons/react/24/outline"
+} from "@heroicons/react/24/solid"
 import { useEffect, useState } from "react"
 import Button from "../button"
 
 import { useAccount } from "wagmi"
 
+import { ShoppingCartIcon } from "@heroicons/react/24/solid"
+
 import useContract from "@/hooks/useContract"
 import useBalance from "@/hooks/useBalance"
+
+import { toast } from "react-toastify"
+import { useConnectionStatus, ConnectWallet } from "@thirdweb-dev/react"
+import classNames from "classnames"
+import { ethers } from "ethers"
+import Modal from "../modal"
+import ModalLoading from "./modalLoading"
+import ModalSuccess from "./modalSuccess"
 
 interface Props {
     setShowChart: (value: boolean) => void
@@ -23,11 +33,24 @@ interface Props {
 
 export default function Buy({ setShowChart, showChart }: Props) {
     const [price, setPrice] = useState<any>(0)
-    const { read, write } = useContract("Commodity")
+    const { read, write, contractAddress, hash } = useContract("Commodity")
     const { address } = useAccount()
 
-    const [selectedToken, setSelectedToken] = useState<Token>(tokens[0])
-    const { read: readToken } = useContract(selectedToken.symbol)
+    const connectionStatus = useConnectionStatus()
+    const isConnected = connectionStatus === "connected"
+
+    const [selectedToken, setSelectedToken] = useState<Token>({} as Token)
+    const [modal, setModal] = useState("")
+
+    useEffect(() => {
+        setSelectedToken(tokens[0])
+    }, [])
+
+    const {
+        read: readToken,
+        contractAddress: tokenAddress,
+        write: tokenInteraction
+    } = useContract(selectedToken.symbol)
 
     const { fetchBalance } = useBalance(selectedToken.symbol)
     const [userBalance, setUserBalance] = useState(0)
@@ -53,7 +76,6 @@ export default function Buy({ setShowChart, showChart }: Props) {
     useEffect(() => {
         async function getBuyPrice() {
             const res = await read("getBuyPrice")
-
             setPrice(Number(res) / 10 ** 18)
         }
 
@@ -70,8 +92,100 @@ export default function Buy({ setShowChart, showChart }: Props) {
         setUsdValue(Math.ceil(numberAmount * price).toString())
     }
 
+    async function handleApprove() {
+        setModal("approving")
+        const decimals = await readToken("decimals")
+        const formattedValue = ethers.utils.parseUnits(
+            usdValue,
+            Number(decimals)
+        )
+
+        if (!contractAddress || !formattedValue) return
+        if (Number(usdValue) < 10) {
+            toast.error("Value in USD must be greather than U$ 10")
+        }
+
+        try {
+            await tokenInteraction("approve", contractAddress, formattedValue)
+
+            setModal("confirm")
+        } catch (e) {
+            toast.error(`Failed to approve: ${e}`)
+            setModal("")
+        }
+    }
+
+    async function handleDeploy() {
+        setModal("loading")
+        const decimals = await readToken("decimals")
+        const formattedValue = ethers.utils.parseUnits(
+            usdValue,
+            Number(decimals)
+        )
+
+        try {
+            // TODO: Save response tx to drawer
+            const response = await write(
+                "createFuture",
+                tokenAddress,
+                formattedValue,
+                {
+                    gasLimit: 10000000
+                }
+            )
+
+            setModal("success")
+        } catch (e) {
+            toast.error(`Error in contract purchase: ${e}`)
+            setModal("confirm")
+        }
+    }
+
     return (
         <div className="flex flex-col items-start gap-2 max-md:w-full max-w-2xl mx-auto">
+            <ModalLoading
+                open={modal === "loading" || modal === "approving"}
+                message={
+                    modal === "loading"
+                        ? {
+                              "line-1": "Wait for confirmation...",
+                              "line-2": `${usdValue} USDT for ${kgAmount} Kg.`,
+                              "line-3": "Confirm the transaction in your wallet"
+                          }
+                        : {
+                              "line-1": "Wait for approval...",
+                              "line-2": `${usdValue} USDT for ${kgAmount} Kg.`,
+                              "line-3": `You must approve the ${selectedToken.symbol} spending in your wallet.`
+                          }
+                }
+            />
+            <ModalSuccess
+                open={modal === "success"}
+                handleClose={() => {
+                    setModal("")
+                    window.open(`https://polygonscan.com/tx/${hash}`, "_blank")
+                }}
+            />
+            <Modal
+                open={modal === "confirm"}
+                title="Confirm your purchase"
+                message="Your purchase is being processed. Confirm to continue."
+                icon={
+                    <ShoppingCartIcon className="text-brand/secondary/500 dark:text-brand/primary/500" />
+                }
+                buttons={[
+                    {
+                        label: "Confirm",
+                        onClick: () => handleDeploy()
+                    },
+                    {
+                        label: "Cancel",
+                        onClick: () => setModal(""),
+                        bgColor: "secondary"
+                    }
+                ]}
+            />
+
             <div className="p-1.5 bg-slate/100 rounded-md dark:bg-deep-gray/500">
                 <button className="bg-base/white py-1.5 px-3.5 rounded text-sm text-gray/900 dark:bg-deep-gray/100 dark:text-gray/300">
                     Buy
@@ -160,7 +274,51 @@ export default function Buy({ setShowChart, showChart }: Props) {
                             }}
                         />
                     </div>
-                    <Button bgColor="primary">Confirm</Button>
+                    {isConnected ? (
+                        <Button
+                            bgColor="primary"
+                            onClick={() => handleApprove()}
+                        >
+                            Buy contract
+                        </Button>
+                    ) : (
+                        <ConnectWallet
+                            className={classNames({
+                                [`w-full
+                                justify-center
+                                inline-flex
+                                py-2
+                                px-4
+                                items-center
+                                gap-[10px]
+                                text-sm
+                                not-italic
+                                font-medium
+                                rounded-[4px]
+                                text-base/white
+                                bg-brand/secondary/500
+                                hover:bg-brand/secondary/600
+                                focus:outline-brand/secondary/300
+                                focus:outline
+                                focus:outline-2
+                                focus:outline-offset-1
+                                cursor-pointer
+
+                                disabled:bg-brand/secondary/100
+                                disabled:text-brand/secondary/300
+                                disabled:cursor-default
+
+                                dark:bg-brand/primary/500
+                                dark:text-deep-gray/100
+                                dark:hover:bg-brand/primary/400
+                                dark:focus:outline-brand/primary/200
+
+                                dark:disabled:text-brand/primary/500
+                                dark:disabled:bg-brand/primary/200
+                                `]: true
+                            })}
+                        />
+                    )}
                 </div>
             </div>
             <div className="w-full text-center mt-2">
