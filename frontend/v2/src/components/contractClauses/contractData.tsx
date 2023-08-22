a229a7c87745dcf65b277f60a0ce802f0305c82a
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-08-21 21:12:25 -0300

    feat(contractClauses):new component will receive contract information

---
+++ frontend/v2/src/components/contractClauses/contractData.tsx
@@ -0,0 +1,272 @@
+"use client"
+export interface ContractSection {
+  id: string;
+  title: string;
+  content: string;
+}
+
+const ContractData: ContractSection[] = [
+  {
+    id: "members",
+    title: "Members agreement in the form of production cooperation by ExtractoDAO Smart Contracts",
+    content: `ExtractoDAO is a privately owned Decentralized
+    Autonomous Organization, sometimes called Decentralized
+    Autonomous Corporation, owner of shared purpose
+    blockchain and manager of a collective smart contract by
+    DAO on Drawer technology that stores several individual
+    smart contracts under the responsibility of each
+    participant that is the contract auditor and issuer of
+    the stablecon COW; And for the purposes of this document
+    represented by the owner Joel Almeida creator, holder,
+    issuer and holder In compliance with all laws of this
+    country. ExtractoDAO It is virtually located in the
+    Domain www.extractodao.com. ExtractoDAO is the rightful
+    issuer of the ExtractoDAO Bull XBLL governance token
+    contract 0x8110706a399D457D67b7A2B7636482b4bfCeBB21,
+    issuer and keeper of the BULL tokens (XBLL) In
+    conformity with all the laws of this. On the other hand,
+    the holder of this smart contract is the legal
+    administrator of the private keys and is fully
+    responsible for managing the funds and generating the
+    COW that will be private and sent back to the original
+    wallet. Smart contract address:
+    0x0b32337D35f8CAB81180b031D9A244E088d0c926`,
+  },
+  {
+    id: "terms",
+    title: "Terms and conditions",
+    content: `ExtractoDAO is a privately owned Decentralized
+    Autonomous Organization, sometimes called Decentralized
+    Autonomous Corporation, owner of shared purpose
+    blockchain and manager of a collective smart contract by
+    DAO on Drawer technology that stores several individual
+    smart contracts under the responsibility of each
+    participant that is the contract auditor and issuer of
+    the stablecon COW; And for the purposes of this document
+    represented by the owner Joel Almeida creator, holder,
+    issuer and holder In compliance with all laws of this
+    country. ExtractoDAO It is virtually located in the
+    Domain www.extractodao.com. ExtractoDAO is the rightful
+    issuer of the ExtractoDAO Bull XBLL governance token
+    contract 0x8110706a399D457D67b7A2B7636482b4bfCeBB21,
+    issuer and keeper of the BULL tokens (XBLL) In
+    conformity with all the laws of this. On the other hand,
+    the holder of this smart contract is the legal
+    administrator of the private keys and is fully
+    responsible for managing the funds and generating the
+    COW that will be private and sent back to the original
+    wallet. Smart contract address:
+    0x0b32337D35f8CAB81180b031D9A244E088d0c926`,
+  },
+  {
+    id: "object",
+    title: "Object of the agreement",
+    content: `ExtractoDAO is a privately owned Decentralized
+    Autonomous Organization, sometimes called Decentralized
+    Autonomous Corporation, owner of shared purpose
+    blockchain and manager of a collective smart contract by
+    DAO on Drawer technology that stores several individual
+    smart contracts under the responsibility of each
+    participant that is the contract auditor and issuer of
+    the stablecon COW; And for the purposes of this document
+    represented by the owner Joel Almeida creator, holder,
+    issuer and holder In compliance with all laws of this
+    country. ExtractoDAO It is virtually located in the
+    Domain www.extractodao.com. ExtractoDAO is the rightful
+    issuer of the ExtractoDAO Bull XBLL governance token
+    contract 0x8110706a399D457D67b7A2B7636482b4bfCeBB21,
+    issuer and keeper of the BULL tokens (XBLL) In
+    conformity with all the laws of this. On the other hand,
+    the holder of this smart contract is the legal
+    administrator of the private keys and is fully
+    responsible for managing the funds and generating the
+    COW that will be private and sent back to the original
+    wallet. Smart contract address:
+    0x0b32337D35f8CAB81180b031D9A244E088d0c926`,
+  },
+  {
+    id: "with",
+    title: "Withdrawal",
+    content: `ExtractoDAO is a privately owned Decentralized
+    Autonomous Organization, sometimes called Decentralized
+    Autonomous Corporation, owner of shared purpose
+    blockchain and manager of a collective smart contract by
+    DAO on Drawer technology that stores several individual
+    smart contracts under the responsibility of each
+    participant that is the contract auditor and issuer of
+    the stablecon COW; And for the purposes of this document
+    represented by the owner Joel Almeida creator, holder,
+    issuer and holder In compliance with all laws of this
+    country. ExtractoDAO It is virtually located in the
+    Domain www.extractodao.com. ExtractoDAO is the rightful
+    issuer of the ExtractoDAO Bull XBLL governance token
+    contract 0x8110706a399D457D67b7A2B7636482b4bfCeBB21,
+    issuer and keeper of the BULL tokens (XBLL) In
+    conformity with all the laws of this. On the other hand,
+    the holder of this smart contract is the legal
+    administrator of the private keys and is fully
+    responsible for managing the funds and generating the
+    COW that will be private and sent back to the original
+    wallet. Smart contract address:
+    0x0b32337D35f8CAB81180b031D9A244E088d0c926`,
+  },
+  {
+    id: "sale",
+    title: "Terms of sale",
+    content: `ExtractoDAO is a privately owned Decentralized
+    Autonomous Organization, sometimes called Decentralized
+    Autonomous Corporation, owner of shared purpose
+    blockchain and manager of a collective smart contract by
+    DAO on Drawer technology that stores several individual
+    smart contracts under the responsibility of each
+    participant that is the contract auditor and issuer of
+    the stablecon COW; And for the purposes of this document
+    represented by the owner Joel Almeida creator, holder,
+    issuer and holder In compliance with all laws of this
+    country. ExtractoDAO It is virtually located in the
+    Domain www.extractodao.com. ExtractoDAO is the rightful
+    issuer of the ExtractoDAO Bull XBLL governance token
+    contract 0x8110706a399D457D67b7A2B7636482b4bfCeBB21,
+    issuer and keeper of the BULL tokens (XBLL) In
+    conformity with all the laws of this. On the other hand,
+    the holder of this smart contract is the legal
+    administrator of the private keys and is fully
+    responsible for managing the funds and generating the
+    COW that will be private and sent back to the original
+    wallet. Smart contract address:
+    0x0b32337D35f8CAB81180b031D9A244E088d0c926`,
+  },
+  {
+    id: "scope",
+    title: "Scope of terms",
+    content: `ExtractoDAO is a privately owned Decentralized
+    Autonomous Organization, sometimes called Decentralized
+    Autonomous Corporation, owner of shared purpose
+    blockchain and manager of a collective smart contract by
+    DAO on Drawer technology that stores several individual
+    smart contracts under the responsibility of each
+    participant that is the contract auditor and issuer of
+    the stablecon COW; And for the purposes of this document
+    represented by the owner Joel Almeida creator, holder,
+    issuer and holder In compliance with all laws of this
+    country. ExtractoDAO It is virtually located in the
+    Domain www.extractodao.com. ExtractoDAO is the rightful
+    issuer of the ExtractoDAO Bull XBLL governance token
+    contract 0x8110706a399D457D67b7A2B7636482b4bfCeBB21,
+    issuer and keeper of the BULL tokens (XBLL) In
+    conformity with all the laws of this. On the other hand,
+    the holder of this smart contract is the legal
+    administrator of the private keys and is fully
+    responsible for managing the funds and generating the
+    COW that will be private and sent back to the original
+    wallet. Smart contract address:
+    0x0b32337D35f8CAB81180b031D9A244E088d0c926`,
+  },
+  {
+    id: "recognition",
+    title: "Recognition and assumption of risks",
+    content: `ExtractoDAO is a privately owned Decentralized
+    Autonomous Organization, sometimes called Decentralized
+    Autonomous Corporation, owner of shared purpose
+    blockchain and manager of a collective smart contract by
+    DAO on Drawer technology that stores several individual
+    smart contracts under the responsibility of each
+    participant that is the contract auditor and issuer of
+    the stablecon COW; And for the purposes of this document
+    represented by the owner Joel Almeida creator, holder,
+    issuer and holder In compliance with all laws of this
+    country. ExtractoDAO It is virtually located in the
+    Domain www.extractodao.com. ExtractoDAO is the rightful
+    issuer of the ExtractoDAO Bull XBLL governance token
+    contract 0x8110706a399D457D67b7A2B7636482b4bfCeBB21,
+    issuer and keeper of the BULL tokens (XBLL) In
+    conformity with all the laws of this. On the other hand,
+    the holder of this smart contract is the legal
+    administrator of the private keys and is fully
+    responsible for managing the funds and generating the
+    COW that will be private and sent back to the original
+    wallet. Smart contract address:
+    0x0b32337D35f8CAB81180b031D9A244E088d0c926`,
+  },
+  {
+    id: "security",
+    title: "Security",
+    content: `ExtractoDAO is a privately owned Decentralized
+    Autonomous Organization, sometimes called Decentralized
+    Autonomous Corporation, owner of shared purpose
+    blockchain and manager of a collective smart contract by
+    DAO on Drawer technology that stores several individual
+    smart contracts under the responsibility of each
+    participant that is the contract auditor and issuer of
+    the stablecon COW; And for the purposes of this document
+    represented by the owner Joel Almeida creator, holder,
+    issuer and holder In compliance with all laws of this
+    country. ExtractoDAO It is virtually located in the
+    Domain www.extractodao.com. ExtractoDAO is the rightful
+    issuer of the ExtractoDAO Bull XBLL governance token
+    contract 0x8110706a399D457D67b7A2B7636482b4bfCeBB21,
+    issuer and keeper of the BULL tokens (XBLL) In
+    conformity with all the laws of this. On the other hand,
+    the holder of this smart contract is the legal
+    administrator of the private keys and is fully
+    responsible for managing the funds and generating the
+    COW that will be private and sent back to the original
+    wallet. Smart contract address:
+    0x0b32337D35f8CAB81180b031D9A244E088d0c926`,
+  },
+  {
+    id: "taxation",
+    title: "Taxation",
+    content: `ExtractoDAO is a privately owned Decentralized
+    Autonomous Organization, sometimes called Decentralized
+    Autonomous Corporation, owner of shared purpose
+    blockchain and manager of a collective smart contract by
+    DAO on Drawer technology that stores several individual
+    smart contracts under the responsibility of each
+    participant that is the contract auditor and issuer of
+    the stablecon COW; And for the purposes of this document
+    represented by the owner Joel Almeida creator, holder,
+    issuer and holder In compliance with all laws of this
+    country. ExtractoDAO It is virtually located in the
+    Domain www.extractodao.com. ExtractoDAO is the rightful
+    issuer of the ExtractoDAO Bull XBLL governance token
+    contract 0x8110706a399D457D67b7A2B7636482b4bfCeBB21,
+    issuer and keeper of the BULL tokens (XBLL) In
+    conformity with all the laws of this. On the other hand,
+    the holder of this smart contract is the legal
+    administrator of the private keys and is fully
+    responsible for managing the funds and generating the
+    COW that will be private and sent back to the original
+    wallet. Smart contract address:
+    0x0b32337D35f8CAB81180b031D9A244E088d0c926`,
+  },
+  {
+    id: "warranties",
+    title: "Representation and warranties",
+    content: `ExtractoDAO is a privately owned Decentralized
+    Autonomous Organization, sometimes called Decentralized
+    Autonomous Corporation, owner of shared purpose
+    blockchain and manager of a collective smart contract by
+    DAO on Drawer technology that stores several individual
+    smart contracts under the responsibility of each
+    participant that is the contract auditor and issuer of
+    the stablecon COW; And for the purposes of this document
+    represented by the owner Joel Almeida creator, holder,
+    issuer and holder In compliance with all laws of this
+    country. ExtractoDAO It is virtually located in the
+    Domain www.extractodao.com. ExtractoDAO is the rightful
+    issuer of the ExtractoDAO Bull XBLL governance token
+    contract 0x8110706a399D457D67b7A2B7636482b4bfCeBB21,
+    issuer and keeper of the BULL tokens (XBLL) In
+    conformity with all the laws of this. On the other hand,
+    the holder of this smart contract is the legal
+    administrator of the private keys and is fully
+    responsible for managing the funds and generating the
+    COW that will be private and sent back to the original
+    wallet. Smart contract address:
+    0x0b32337D35f8CAB81180b031D9A244E088d0c926`,
+  },
+
+];
+
+export default ContractData;
