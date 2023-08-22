497d62a321270a874faeddbf52a954740d434e52
Robson <robson.miranda1991@gmail.com>
2023-08-22 10:37:27 -0300

    feat: next environment vars

--- frontend/v2/next.config.js
+++ frontend/v2/next.config.js
@@ -4,11 +4,15 @@ const nextConfig = {
     env: {
         ALCHEMY_ID: process.env.ALCHEMY_ID,
         API_URL: process.env.API_URL,
+        BACKEND_ADDRESS: process.env.BACKEND_ADDRESS,
         GRAPHQL_URL: process.env.GRAPHQL_URL,
         CHAIN_ID: process.env.CHAIN_ID,
         SIWE_VERSION: process.env.SIWE_VERSION,
         SIWE_STATEMENT: process.env.SIWE_STATEMENT,
-        DOMAIN: process.env.DOMAIN
+        DOMAIN: process.env.DOMAIN,
+        SIGNOUT_MESSAGE: process.env.SIGNOUT_MESSAGE,
+        SIGNIN_MESSAGE: process.env.SIGNIN_MESSAGE,
+        ORIGIN: process.env.ORIGIN
     }
 }
 
