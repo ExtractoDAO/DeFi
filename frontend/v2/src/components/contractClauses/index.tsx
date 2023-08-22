7b71ea992e9e226ecb6d31ebd3b84b8a39a84ee7
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-08-21 21:15:06 -0300

    feat(contractClauses):Component responsible for presenting a contract to the user

---
+++ frontend/v2/src/components/contractClauses/index.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import Link from "next/link";
+import React from "react";
+import ContractData from "./contractData";
+
+const ContractClauses = () => {
+  return (
+    <div className="flex">
+      <div className="text-white max-w-[120rem] mr-3">
+        <h1 className="text-2xl">Sumário</h1>
+        <ol>
+          {ContractData.map((section) => (
+            <li key={section.id} className="mb-2 border border-gray/500">
+              <Link href={`#${section.id}`}>{section.title}</Link>
+            </li>
+          ))}
+        </ol>
+      </div>
+      <div id="contract">
+        <h1>CONTRATO</h1>
+        <div className="scroll-smooth overflow-y-auto border border-gray/500 text-white h-[600px] w-[600px]">
+          {ContractData.map((section) => (
+            <div key={section.id}>
+              <h2 id={section.id}>{section.title}</h2>
+              <p className="text-red/500">{section.content}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContractClauses;
