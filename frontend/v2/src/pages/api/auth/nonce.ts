cabb84aec28702ee0dc633b9b8dee0351eeb832d
Robson <robson.miranda1991@gmail.com>
2023-08-23 14:45:18 -0300

    feat: thirdweb sign

--- frontend/v2/src/pages/api/auth/nonce.ts
+++ frontend/v2/src/pages/api/auth/nonce.ts
@@ -1,12 +1,22 @@
+import AxiosService from "@/services/axios"
+import env from "@/services/environment"
+import { AxiosError } from "axios"
 import { NextApiRequest, NextApiResponse } from "next"
+
+const { BACKEND_ADDRESS } = env
+const axiosInstance = new AxiosService(env)
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const response = await fetch(
-        `${process.env.API_URL}/login/nonce/${req.query.address}`
-    )
-    const data = await response.json()
-    res.status(200).json(data)
+    try {
+        const { address } = req.query
+        const route = `${BACKEND_ADDRESS}/login/nonce/${address}`
+        const data = await axiosInstance.get(route)
+
+        return res.status(200).json(data)
+    } catch (e: any) {
+        res.status(e.response.status).json(e.response.data)
+    }
 }
