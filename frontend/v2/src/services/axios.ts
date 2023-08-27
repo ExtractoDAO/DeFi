bf878c2267d22aa84dce6094c30678b9f583e9c9
Robson <robson.miranda1991@gmail.com>
2023-08-27 12:06:27 -0300

    fix: axios instance

--- frontend/v2/src/services/axios.ts
+++ frontend/v2/src/services/axios.ts
@@ -1,44 +1,11 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios"
-import { Environment } from "./environment"
+import axios from "axios"
 
-export default class AxiosService {
-    private instance: AxiosInstance
-
-    constructor(env: Environment) {
-        this.instance = axios.create({
-            baseURL: env.BACKEND_ADDRESS,
-            timeout: 5000,
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        this.initializeResponseInterceptor()
+const api = axios.create({
+    headers: {
+        "Content-Type": "application/json"
     }
-    private handleResponse = ({ data }: AxiosResponse) => data
-    private handleError = (error: any) => Promise.reject(error)
-    private initializeResponseInterceptor = () => {
-        this.instance.interceptors.response.use(
-            this.handleResponse,
-            this.handleError
-        )
-    }
+})
 
-    public async get(url: string, config?: AxiosRequestConfig) {
-        try {
-            const response = await this.instance.get(url, config)
+// api.defaults.headers.common["Authorization"] = `Bearer ${getToken()}`
 
-            return response
-        } catch (error) {
-            throw error
-        }
-    }
-
-    public async post(url: string, data?: any, config?: AxiosRequestConfig) {
-        try {
-            const response = await this.instance.post(url, data, config)
-            return response
-        } catch (error) {
-            throw error
-        }
-    }
-}
+export default api
