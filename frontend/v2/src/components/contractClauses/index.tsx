336b4823490a1a9ba28531ee7bfa16f7fe5a7b7b
Lucasccgomes <lucasccgomes@protonmail.com>
2023-09-13 21:54:17 -0300

    chore(contract-clauses):I corrected the background of the summary, added a style to the contract display container, started styling the titles and paragraphs of the contact

--- frontend/v2/src/components/contractClauses/index.tsx
+++ frontend/v2/src/components/contractClauses/index.tsx
@@ -8,8 +8,8 @@ const ContractClauses = () => {
     return (
         <div className="flex">
             <div
-                className="
-                text-gray/900
+                className="text-gray/900
+                  bg-white
                     max-w-[312px] 
                     flex
                     items-start
@@ -51,79 +51,90 @@ const ContractClauses = () => {
                                 <li
                                     key={section.id}
                                     className="flex
-                            
-                                    max-w-[264px]
-                                    pb-1
-                                    "
-                                >
+                                        max-w-[264px]
+                                        pb-1
+                                            ">
                                     <Link
                                         className="
-                                        hover:bg-slate/200
-                                        focus:bg-slate/100
-                                        focus:text-gray/900
-                                        py-2
-                                        px-2
-                                        items-start
-                                        w-full
-                                        rounded
-                                        text-gray/500
+                                            hover:bg-slate/200
+                                            focus:bg-slate/100
+                                            focus:text-gray/900
+                                            py-2
+                                            px-2
+                                            items-start
+                                            w-full
+                                            rounded
+                                            text-gray/500
                                     
-                                        dark:hover:bg-clauses/section/hover
-                                        dark:focus:bg-clauses/section/active"
+                                            dark:hover:bg-clauses/section/hover
+                                            dark:focus:bg-clauses/section/active"
                                         href={`#${section.id}`}
                                     >
                                         {section.title}
                                     </Link>
                                 </li>
-                            ))}
+                            ))} 
                         </ol>
                     </div>
                 </div>
             </div>
             <div id="contract" className="flex
-                                w-[71%]
-                                max-h-[603px]
-                                flex-col
-                                items-start
-                                gap-10
-                                shrink-0
-                                rounded
-                                border
-                                border-solid
-                                border-gray/200
-                                box-shadow
-                                bg-white
-                                p-6
-                                ml-6
-            ">
+                                    w-[71%]
+                                    max-h-[603px]
+                                    flex-col
+                                    items-start
+                                    gap-10
+                                    shrink-0
+                                    rounded
+                                    border
+                                    border-solid
+                                    border-gray/200
+                                    box-shadow
+                                    bg-white
+                                    p-6
+                                    ml-6
+                                    ">
                 <h1 className="text-2xl
-                    not-italic
-                    leading-normal
-                    font-semibold
-                    text-gray/900
-                
+                        not-italic
+                        leading-normal
+                        font-semibold
+                        text-gray/900
                 ">CONTRATO</h1>
                 <div className="scroll-smooth
-                     border-0
-                     overflow-y-auto 
-                     border-gray/500 
-                     text-white 
-                     h-full
-                     w-full
-                     flex
-                     flex-col
-                     items-start
-                     flex-[1_0_0]
-                     gap-10
+                        scroll
+                        scrolllight
+                        border-0
+                        overflow-y-auto 
+                        border-gray/500 
+                        text-white 
+                        h-full
+                        w-full
+                        flex
+                        flex-col
+                        items-start
+                        flex-[1_0_0]
+                        gap-10
+                        pr-3
+
+                        dark:scrolldark
                      ">
                     {ContractData.map((section) => (
-                        <div key={section.id}>
+                        <div key={section.id} className="flex
+                                                  flex-col
+                                                  gap-4
+                                                  items-start">
                             <h2 id={section.id} className="text-gray/900
-                                                text-xl
-                                                not-italic
-                                                font-semibold
-                                                leading-normal">{section.title}</h2>
-                            <p className="text-red/500">{section.content}</p>
+                                                    text-xl
+                                                    not-italic
+                                                    font-semibold
+                                                    leading-normal
+                                                ">{section.title}</h2>
+                            <p className="items-stretch
+                                    text-sm
+                                    font-normal
+                                    not-italic
+                                    text-gray/600
+                               ">{section.content}</p>
                         </div>
                     ))}
                 </div>
