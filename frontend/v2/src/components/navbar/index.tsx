08aef182de4dbe5d04b6d1370ab312aaefb799cc
Robson <robson.miranda1991@gmail.com>
2023-09-16 10:54:02 -0300

    add: useAuth hook

--- frontend/v2/src/components/navbar/index.tsx
+++ frontend/v2/src/components/navbar/index.tsx
@@ -1,143 +1,33 @@
 "use client"
-import React, { useEffect, useMemo, useState } from "react"
+import React from "react"
 import { Cog8ToothIcon } from "@heroicons/react/24/outline"
 import { BellIcon } from "@heroicons/react/24/outline"
 import Link from "next/link"
 import classNames from "classnames"
-import { usePathname } from "next/navigation"
+
 import LogoDAO from "@/assets/img/logo-dao.svg"
 import Image from "next/image"
 
-import {
-    ConnectWallet,
-    useAddress,
-    useConnectionStatus,
-    useSDK
-} from "@thirdweb-dev/react"
+import { ConnectWallet } from "@thirdweb-dev/react"
 
 import { LockClosedIcon } from "@heroicons/react/24/outline"
 
-import Modal from "../modal"
-import { deleteItem, getItem, setItem } from "@/services/storage"
-
 import { shortAddress } from "@/utils/mask"
-
-import { Login } from "@/services/backend/login"
-import env from "@/services/environment"
-import AxiosService from "@/services/axios"
-
 import { switchTheme } from "@/utils/theme"
-import { Backend } from "@/services/backend/backend"
-import Button from "../button"
+import useAuth from "@/hooks/useAuth"
 
-const pathnames: Dictionary = {
-    "/": "Dashboard",
-    "/buy": "Buy contracts",
-    "/drawer": "Drawer",
-    "/exchange": "Exchange"
-}
+import Modal from "../modal"
 
 export default function Navbar() {
-    const savedToken = getItem("TOKEN")
-    const sdk = useSDK()
-
-    const backend = new Backend(env, new AxiosService(env))
-    const login = new Login(env, new AxiosService(env))
-    const address = useAddress()
-    const pathname = usePathname()
-    const pageTitle = pathnames[pathname || "/"]
-    const connectionStatus = useConnectionStatus()
-    const [isConnected, setIsConnected] = useState(false)
-    const [modalSign, setModalSign] = useState(false)
-    const [showNotificationIcon, setShowNotificationIcon] = useState(false)
-    const [loading, setLoading] = useState(false)
-
-    useEffect(() => {
-        const oldState = isConnected
-        const newState = connectionStatus === "connected"
-
-        if (oldState === true && newState === false) {
-            signout()
-        }
-
-        setIsConnected(newState)
-        setShowNotificationIcon(newState)
-    }, [connectionStatus])
-
-    useEffect(() => {
-        const newState = connectionStatus === "connected"
-
-        if (newState && address && !savedToken) {
-            setModalSign(true)
-        }
-    }, [address, connectionStatus])
-
-    const signout = async () => {
-        if (!address || !savedToken || !sdk?.wallet.sign) {
-            return
-        }
-
-        const isSuccess = await login.signOut({
-            address
-        })
-
-        if (isSuccess) {
-            deleteItem("TOKEN")
-            deleteItem("TOKEN_EXPIRES_AT")
-        }
-    }
-
-    const sign = async () => {
-        if (address && sdk?.wallet.sign) {
-            setLoading(true)
-            const { token, expirationTime } = await backend.login.signIn({
-                address,
-                signMessage: (message: string) => sdk?.wallet.sign(message)
-            })
-
-            setModalSign(false)
-
-            setItem("TOKEN", token)
-            setItem("TOKEN_EXPIRES_AT", expirationTime)
-            setLoading(false)
-        }
-    }
-
-    const handleSaveTestContract = () => {
-        backend.graphql
-            .ordersByInvestor("0xf0862d81bd4c6ccc115536d19ac0f5e6b1b02d71")
-            .then((r) => {
-                console.log(r)
-            })
-        backend.graphql
-            .ordersByHash(
-                "0x42bce2258ab1ebe311d4a8af4ae58e304d1c5f0951d331cc6e3ce6f68501036c"
-            )
-            .then((r) => {
-                console.log(r)
-            })
-        backend.graphql.orders().then((r) => {
-            console.log(r)
-        })
-        backend.graphql.contracts().then((r) => {
-            console.log(r)
-        })
-        backend.graphql.investors().then((r) => {
-            console.log(r)
-        })
-        backend.graphql.contractByAddress("0xff").then((r) => {
-            console.log(r)
-        })
-        backend.graphql.contractByInvestor("0xff").then((r) => {
-            console.log(r)
-        })
-        backend.graphql.buyPrices().then((r) => {
-            console.log(r)
-        })
-        backend.graphql.sellPrices().then((r) => {
-            console.log(r)
-        })
-    }
+    const {
+        loading,
+        modalSign,
+        pageTitle,
+        showNotificationIcon,
+        sign,
+        address,
+        setModalSign
+    } = useAuth()
 
     const ButtonConect = () => (
         <ConnectWallet
@@ -316,9 +206,7 @@ export default function Navbar() {
                                 />
                             </Link>
                         </div>
-                        <Button onClick={handleSaveTestContract}>
-                            Test save Contract
-                        </Button>
+
                         <ButtonConect />
                     </div>
                 </div>
