0001830ca97cc673a1b272ac0820bf7575bcd222
Robson <robson.miranda1991@gmail.com>
2023-10-11 17:18:34 -0300

    add: dashboard table

--- frontend/v2/src/app/page.tsx
+++ frontend/v2/src/app/page.tsx
@@ -1,4 +1,86 @@
 export default async function Home() {
+    const Box = ({
+        title,
+        value,
+        description
+    }: {
+        title: string
+        value: string
+        description: string
+    }) => (
+        <div
+            className="
+                        flex
+                        w-full
+                        p-6
+                        flex-col
+                        gap-4
+                        flex-[1 0 0]
+                        first:rounded-l-lg
+                        last:rounded-r-lg
+                        border
+                        border-solid
+                        bg-brand/secondary/500
+                        border-brand/secondary/600
+                        dark:bg-brand/primary/500
+                        dark:border-brand/primary/500"
+        >
+            <p
+                className="text-gray/300
+                            text-sm
+                            not-italic
+                            font-medium
+                            dark:text-cool-gray/800
+                            "
+            >
+                {title}
+            </p>
+            <div
+                className="flex
+                            items-end
+                            gap-3"
+            >
+                <p
+                    className="text-3xl
+                                font-bold
+                                not-italic
+                                text-white
+                                dark:text-deep-gray/100"
+                >
+                    {value}
+                </p>
+                <p
+                    className="text-gray/300
+                                text-sm
+                                not-italic
+                                font-normal
+                                dark:text-cool-gray/800"
+                >
+                    {description}
+                </p>
+            </div>
+        </div>
+    )
+
+    const Row = () => {
+        return (
+            <tr className="bg-white dark:bg-gray/900 border-b border-Default/gray/200 dark:border-deep-gray/200">
+                <td className="px-6 py-4 text-gray/500 dark:bg-deep-gray/100">
+                    248,85
+                </td>
+                <td className="px-6 py-4 text-gray/500 dark:bg-deep-gray/100">
+                    2,30%
+                </td>
+                <td className="px-6 py-4 text-gray/500 dark:bg-deep-gray/100">
+                    50,51
+                </td>
+                <td className="px-6 py-4 text-gray/500 dark:bg-deep-gray/100">
+                    05-06-2023
+                </td>
+            </tr>
+        )
+    }
+
     return (
         <main>
             <div>
@@ -12,187 +94,27 @@ export default async function Home() {
                     Overview your account
                 </h1>
                 <div
-                    className="flex
-                       w-full"
-                >
-                    <div
-                        className="flex
+                    className="
+                        flex
                         w-full
-                        p-6
-                        flex-col
-                        gap-4
-                        flex-[1 0 0]
-                        rounded-l-lg
-                        border
-                        border-solid
-                        bg-brand/secondary/500
-                        border-brand/secondary/600"
-                    >
-                        <p
-                            className="text-gray/300
-                            text-sm
-                            not-italic
-                            font-medium
-                            "
-                        >
-                            Total Balance
-                        </p>
-                        <div
-                            className="flex
-                            items-end
-                            gap-3"
-                        >
-                            <p
-                                className="text-3xl
-                                font-bold
-                                not-italic
-                                text-white"
-                            >
-                                400
-                            </p>
-                            <p
-                                className="text-gray/300
-                                text-sm
-                                not-italic
-                                font-normal"
-                            >
-                                USDT
-                            </p>
-                        </div>
-                    </div>
-                    <div
-                        className="flex
-                          w-full
-                          p-6
-                          flex-col
-                          gap-4
-                          flex-[1 0 0]
-                          border
-                          border-solid
-                          bg-brand/secondary/500
-                          border-brand/secondary/600"
-                    >
-                        <p
-                            className="text-gray/300
-                            text-sm
-                            not-italic
-                            font-medium
-                            "
-                        >
-                            Number of contracts
-                        </p>
-                        <div
-                            className="flex
-                            items-end
-                            gap-3"
-                        >
-                            <p
-                                className="text-3xl
-                                font-bold
-                                not-italic
-                                text-white"
-                            >
-                                30
-                            </p>
-                            <p
-                                className="text-gray/300
-                                text-sm
-                                not-italic
-                                font-normal"
-                            >
-                                contracts pruchased
-                            </p>
-                        </div>
-                    </div>
-                    <div
-                        className="flex
-                          w-full
-                          p-6
-                          flex-col
-                          gap-4
-                          flex-[1 0 0]
-                          border
-                          border-solid
-                          bg-brand/secondary/500
-                          border-brand/secondary/600"
-                    >
-                        <p
-                            className="text-gray/300
-                            text-sm
-                            not-italic
-                            font-medium
-                            "
-                        >
-                            Avaliable for withdrawal
-                        </p>
-                        <div
-                            className="flex
-                            items-end
-                            gap-3"
-                        >
-                            <p
-                                className="text-3xl
-                                font-bold
-                                not-italic
-                                text-white"
-                            >
-                                05
-                            </p>
-                            <p
-                                className="text-gray/300
-                                text-sm
-                                not-italic
-                                font-normal"
-                            >
-                                contracts avaliable
-                            </p>
-                        </div>
-                    </div>
-                    <div
-                        className="flex
-                          w-full
-                          p-6
-                          flex-col
-                          gap-4
-                          flex-[1 0 0]
-                          rounded-r-lg
-                          border
-                          border-solid
-                          bg-brand/secondary/500
-                          border-brand/secondary/600"
-                    >
-                        <p
-                            className="text-gray/300
-                            text-sm
-                            not-italic
-                            font-medium
-                            "
-                        >
-                            Value fat ox
-                        </p>
-                        <div
-                            className="flex
-                            items-end
-                            gap-3"
-                        >
-                            <p
-                                className="text-3xl
-                                font-bold
-                                not-italic
-                                text-white"
-                            >
-                                50,51
-                            </p>
-                            <p
-                                className="text-gray/300
-                                text-sm
-                                not-italic
-                                font-normal"
-                            >
-                                Dollar
-                            </p>
-                        </div>
-                    </div>
+                    "
+                >
+                    <Box title="Total Balance" value="400" description="USDT" />
+                    <Box
+                        title="Number of contracts"
+                        value="30"
+                        description="contracts pruchased"
+                    />
+                    <Box
+                        title="Avaliable for withdrawal"
+                        value="05"
+                        description="contracts avaliable"
+                    />
+                    <Box
+                        title="Value fat ox"
+                        value="50,51"
+                        description="Dollar"
+                    />
                 </div>
 
                 <div className="relative overflow-x-auto ">
@@ -206,156 +128,27 @@ export default async function Home() {
                     >
                         Fat ox price historic
                     </h1>
-                    <table
-                        className="w-full
-                            text-sm
-                            text-left
-                            text-gray-500
-                            border
-                            border-gray/200
-                            rounded
-                            "
-                    >
-                        <thead
-                            className="h-[38px]
-                                justify-center
-                                items-start
-                                self-stretch
-                                text-gray/500
-                                font-medium
-                                not-italic
-                                uppercase
-                                text-xs"
-                        >
-                            <tr>
+                    <table className="w-full text-sm text-left">
+                        <thead className="text-xs font-medium text-gray/500 uppercase bg-Default/gray/50 dark:border-deep-gray/200 border-b border-Default/gray/200 dark:bg-deep-gray/700 dark:text-gray/500">
+                            <tr className="border border-Default/gray/200 dark:border-deep-gray/200">
                                 <th scope="col" className="px-6 py-3">
-                                    VALUE
+                                    value
                                 </th>
                                 <th scope="col" className="px-6 py-3">
-                                    VAl./DAY
+                                    VAl./day
                                 </th>
                                 <th scope="col" className="px-6 py-3">
-                                    VALUE US$
+                                    VALue US$
                                 </th>
-                                <th scope="col" className="px-6 py-3"></th>
+                                <th scope="col" className="px-6 py-3">
+                                    &nbsp;
+                                </th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr
-                                className="h-[72px]
-                                px-6
-                                py-4
-                                justify-center
-                                items-start
-                                gap-[10px]
-                                self-stretch
-                                text-gray/500
-                                not-italic
-                                font-normal
-                                text-sm"
-                            >
-                                <th
-                                    scope="row"
-                                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                                >
-                                    1
-                                </th>
-                                <td className="px-6 py-4">2</td>
-                                <td className="px-6 py-4">3</td>
-                                <td className="px-6 py-4">4</td>
-                            </tr>
-                            <tr
-                                className="h-[72px]
-                                px-6
-                                py-4
-                                justify-center
-                                items-start
-                                gap-[10px]
-                                self-stretch
-                                text-gray/500
-                                not-italic
-                                font-normal
-                                text-sm"
-                            >
-                                <th
-                                    scope="row"
-                                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                                >
-                                    53
-                                </th>
-                                <td className="px-6 py-4">34</td>
-                                <td className="px-6 py-4">45</td>
-                                <td className="px-6 py-4">78</td>
-                            </tr>
-                            <tr
-                                className="h-[72px]
-                                px-6
-                                py-4
-                                justify-center
-                                items-start
-                                gap-[10px]
-                                self-stretch
-                                text-gray/500
-                                not-italic
-                                font-normal
-                                text-sm"
-                            >
-                                <th
-                                    scope="row"
-                                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                                >
-                                    89
-                                </th>
-                                <td className="px-6 py-4">90</td>
-                                <td className="px-6 py-4">786</td>
-                                <td className="px-6 py-4">678978</td>
-                            </tr>
-                            <tr
-                                className="h-[72px]
-                                px-6
-                                py-4
-                                justify-center
-                                items-start
-                                gap-[10px]
-                                self-stretch
-                                text-gray/500
-                                not-italic
-                                font-normal
-                                text-sm"
-                            >
-                                <th
-                                    scope="row"
-                                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                                >
-                                    567
-                                </th>
-                                <td className="px-6 py-4">234</td>
-                                <td className="px-6 py-4">897</td>
-                                <td className="px-6 py-4">56768</td>
-                            </tr>
-                            <tr
-                                className="h-[72px]
-                                px-6
-                                py-4
-                                justify-center
-                                items-start
-                                gap-[10px]
-                                self-stretch
-                                text-gray/500
-                                not-italic
-                                font-normal
-                                text-sm"
-                            >
-                                <th
-                                    scope="row"
-                                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                                >
-                                    457656
-                                </th>
-                                <td className="px-6 py-4">5678</td>
-                                <td className="px-6 py-4">56785679</td>
-                                <td className="px-6 py-4">789789</td>
-                            </tr>
+                            {new Array(8).fill("").map((_, i) => {
+                                return <Row key={i} />
+                            })}
                         </tbody>
                     </table>
                 </div>
