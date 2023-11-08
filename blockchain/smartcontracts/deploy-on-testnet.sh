d3a7925ead436e137adaeca14b98b89d19f74b1d
Robson Miranda <46539960+robson036@users.noreply.github.com>
2023-11-08 15:41:12 -0300

    Blockchain/upgrade struct (#143)
    * update: move and reorganize dir struct
    
    * update: building the update deploy script
    
    * feat: deploy testnet
    
    ---------
    
    Co-authored-by: Lucas Oliveira <olivmath@protonmail.com>

--- blockchain/smartcontracts/deploy-on-testnet.sh
+++ blockchain/smartcontracts/deploy-on-testnet.sh
@@ -1,6 +1,7 @@
 source .env
 
-forge script scripts/deploy.dao.testnet.sol:Mumbai \
+forge script scripts/v2/testnet/deploy.s.sol:Testnet \
     --private-key $MUMBAI_PRIVATE_KEY \
     --rpc-url $MUMBAI_RPC_URL \
-    # --broadcast
+    --broadcast \
+    --verbosity
